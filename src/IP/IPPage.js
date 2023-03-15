4be714afa090c922b87420b74b20d585e52e43d0
Eyal Mogenshtein <117386089+EyalMuga@users.noreply.github.com>
2023-03-15 14:55:20 +0200

    draft

--- src/IP/IPPage.js
+++ src/IP/IPPage.js
@@ -9,7 +9,7 @@ import { useState} from "react";
 export default function IPPage() {
     const [ip, setIp] = useState("")
     const [ipInfo, setIpInfo] = useState(null)
-    const [error, setError] = useState(null)
+
 
     const getIpInfo = async () => {
         try {
